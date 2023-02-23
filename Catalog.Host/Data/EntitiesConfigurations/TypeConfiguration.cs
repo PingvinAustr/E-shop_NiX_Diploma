559aae6e25b4c2573f36b815b8eb6d6c2f10432e
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-23 18:31:33 +0200

    feature/added entities configs

---
+++ Catalog.Host/Data/EntitiesConfigurations/TypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.Host.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catalog.Host.Data.EntitiesConfigurations
+{
+    public class TypeConfiguration : IEntityTypeConfiguration<Entities.Type>
+    {
+        public void Configure(EntityTypeBuilder<Entities.Type> builder)
+        {
+            builder.ToTable("Type");
+
+            builder.HasKey(x => x.TypeId);
+
+            builder.Property(x => x.TypeId)
+                .UseHiLo("car_type_hilo")
+                .IsRequired();
+
+            builder.Property(x => x.TypeName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.TypeDescription)
+                .IsRequired()
+                .HasMaxLength(250);
+        }
+    }
+}
