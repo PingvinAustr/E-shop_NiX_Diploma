0b1f76720c9c77b3de5f00fc1d68ef19f8e883e1
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-23 18:31:16 +0200

    feature/added basic entities

---
+++ Catalog.Host/Data/Entities/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.Host.Data.Entities
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string CarName { get; set; }
+        public string CarPromo { get; set; }
+        public int Price { get; set; }
+        public string ImageFileName { get; set; }
+        public int TypeId { get; set; }
+        public Type CarType { get; set; }
+        public int ManufacturerId { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
