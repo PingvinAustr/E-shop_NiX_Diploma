088e9c0de75fff4b4d031b28b5ad06f8cbad4ca6
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 12:58:50 +0200

    feature/added catalog service

---
+++ Catalog.Host/Services/CatalogService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Catalog.Host.Configurations;
+using Catalog.Host.Data;
+using Catalog.Host.Models.Dtos;
+using Catalog.Host.Models.Responses;
+using Catalog.Host.Repositories.Interfaces;
+using Catalog.Host.Services.Interfaces;
+
+namespace Catalog.Host.Services
+{
+    public class CatalogService : BaseDataService<AppDbContext>, ICatalogService
+    {
+        private readonly ICarRepository _catalogItemRepository;
+        private readonly IMapper _mapper;
+
+        public CatalogService(
+            IDbContextWrapper<AppDbContext> dbContextWrapper,
+            ILogger<BaseDataService<AppDbContext>> logger,
+            ICarRepository catalogItemRepository,
+            IMapper mapper)
+            : base(dbContextWrapper, logger)
+        {
+            _catalogItemRepository = catalogItemRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<PaginatedItemsResponse<CarDto>> GetCatalogItemsAsync(int pageSize, int pageIndex)
+        {
+            return await ExecuteSafeAsync(async () =>
+            {
+                var result = await _catalogItemRepository.GetByPageAsync(pageIndex, pageSize);
+                return new PaginatedItemsResponse<CarDto>()
+                {
+                    Count = result.TotalCount,
+                    Data = result.Data.Select(s => _mapper.Map<CarDto>(s)).ToList(),
+                    PageIndex = pageIndex,
+                    PageSize = pageSize
+                };
+            });
+        }
+
+        public async Task<PaginatedItemsResponse<ManufacturerDto>> GetBrands()
+        {
+            return await ExecuteSafeAsync(async () =>
+            {
+                var result = await _catalogItemRepository.GetBrands();
+                return new PaginatedItemsResponse<ManufacturerDto>()
+                {
+                    Count = result.TotalCount,
+                    Data = result.Data.Select(s => _mapper.Map<ManufacturerDto>(s)).ToList(),
+                };
+            });
+        }
+    }
+}
