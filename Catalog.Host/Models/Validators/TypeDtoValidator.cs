875e04d3b2ddba41561661057f224cdb3c3895bf
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 15:20:44 +0200

    feature/added fluent validation validators for dtos

---
+++ Catalog.Host/Models/Validators/TypeDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.Host.Models.Dtos;
+using FluentValidation;
+
+public class TypeDtoValidator : AbstractValidator<TypeDto>
+{
+    public TypeDtoValidator()
+    {
+        RuleFor(x => x.TypeId)
+            .GreaterThan(0)
+            .WithMessage("Type ID must be greater than 0");
+
+        RuleFor(x => x.TypeName)
+            .NotEmpty()
+            .WithMessage("Type name is required")
+            .MaximumLength(100)
+            .WithMessage("Type name must not exceed 100 characters");
+
+        RuleFor(x => x.TypeDescription)
+            .MaximumLength(250)
+            .WithMessage("Type description must not exceed 250 characters");
+    }
+}
