3c62d4a097a64f7d9c6818379105f8b09f2c8103
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 12:57:28 +0200

    feature/added dtos

---
+++ Catalog.Host/Models/Dtos/ManufacturerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Host.Models.Dtos
+{
+    public class ManufacturerDto
+    {
+        public int Id { get; set; }
+        public string ManufacturerName { get; set; }
+        public string ManufacturerCountry { get; set; }
+    }
+}
