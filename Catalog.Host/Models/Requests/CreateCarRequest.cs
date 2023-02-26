7d5135aede825326b755f9d6130696efbf8dd822
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 15:20:17 +0200

    feature/created request models

--- Catalog.Host/Models/Requests/CreateCarRequest.cs
+++ Catalog.Host/Models/Requests/CreateCarRequest.cs
@@ -1,13 +1,26 @@
-﻿namespace Catalog.Host.Models.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Host.Models.Requests
 {
     public class CreateCarRequest
     {
+        [Required(ErrorMessage = "Car name is required")]
+        [MaxLength(100)]
         public string CarName { get; set; }
+
+        [MaxLength(300)]
         public string CarPromo { get; set; }
+
+        [Required(ErrorMessage = "Car price is required")]
         public int Price { get; set; }
         public string ImageFileName { get; set; }
+        [Required(ErrorMessage = "Car type id is required")]
         public int CarTypeId { get; set; }
+
+        [Required(ErrorMessage = "Car manufacturer id is required")]
         public int ManufacturerId { get; set; }
+
+        [Required(ErrorMessage = "Car availability is required")]
         public bool IsAvailable { get; set; }
     }
 }
---
+++ Catalog.Host/Models/Requests/CreateTypeRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Host.Models.Requests
+{
+    public class CreateTypeRequest
+    {
+        [Required(ErrorMessage ="Type name is required")]
+        public string TypeName { get; set; }
+
+        [Required(ErrorMessage = "Type name is required")]
+        public string TypeDescription { get; set; }
+    }
+}
