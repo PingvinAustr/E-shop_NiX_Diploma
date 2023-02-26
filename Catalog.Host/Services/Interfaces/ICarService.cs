3853020121d87c59fc482f6239d5996256eb272c
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 12:58:38 +0200

    feature/added service interfaces

---
+++ Catalog.Host/Services/Interfaces/ICarService.cs
@@ -0,0 +1,7 @@
+﻿namespace Catalog.Host.Services.Interfaces
+{
+    public interface ICarService
+    {
+        Task<int?> Add(string name, string promo, int price, bool isAvailable, int carTypeId, int carManufaturerid, string imageFileName);
+    }
+}
---
+++ Catalog.Host/Services/Interfaces/ICatalogService.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.Host.Models.Dtos;
+using Catalog.Host.Models.Responses;
+
+namespace Catalog.Host.Services.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<PaginatedItemsResponse<CarDto>> GetCatalogItemsAsync(int pageSize, int pageIndex);
+        Task<PaginatedItemsResponse<ManufacturerDto>> GetBrands();
+    }
+}
