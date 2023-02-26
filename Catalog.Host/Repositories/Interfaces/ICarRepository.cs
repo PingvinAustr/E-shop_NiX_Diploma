66f9da9d6766b69e3389d5ebf1b9406b449e6632
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 14:13:24 +0200

    feature/added repo interfaces

--- Catalog.Host/Repositories/Interfaces/ICarRepository.cs
+++ Catalog.Host/Repositories/Interfaces/ICarRepository.cs
@@ -8,5 +8,11 @@ namespace Catalog.Host.Repositories.Interfaces
         Task<PaginatedItems<Car>> GetByPageAsync(int pageIndex, int pageSize);
         Task<int?> Add(string name, string promo, int price, bool isAvailable, int carTypeId, int carManufaturerid, string imageFileName);
         Task<PaginatedItems<Manufacturer>> GetBrands();
+        Task<PaginatedItems<Data.Entities.Type>> GetTypes();
+        Task<PaginatedItems<Car>> GetById(int id);
+        Task<PaginatedItems<Car>> GetByType(int typeId);
+        Task<PaginatedItems<Car>> GetByBrand(int brandId);
+        Task<int?> Put(Car item, int id);
+        bool Delete(int id);
     }
 }
