7b2b7a66ee8ee7fac3086cf1fc190669c42dd36c
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 14:13:36 +0200

    feature/added repo interface

---
+++ Catalog.Host/Repositories/Interfaces/IManufacturerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Data.Entities;
+using Catalog.Host.Models.Dtos;
+namespace Catalog.Host.Repositories.Interfaces;
+
+public interface IManufacturerRepository
+{
+    Task<int?> Put(Manufacturer item, int id);
+    bool Delete(int id);
+    Task<int?> Add(string brand, string country);
+}
