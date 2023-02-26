ce2ffd927bc781220ca70f07701100445f9ee22e
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 14:12:46 +0200

    fix/fixed manufacturer dto

--- Catalog.Host/Models/Dtos/ManufacturerDto.cs
+++ Catalog.Host/Models/Dtos/ManufacturerDto.cs
@@ -2,7 +2,7 @@
 {
     public class ManufacturerDto
     {
-        public int Id { get; set; }
+        public int ManufacturerId { get; set; }
         public string ManufacturerName { get; set; }
         public string ManufacturerCountry { get; set; }
     }
