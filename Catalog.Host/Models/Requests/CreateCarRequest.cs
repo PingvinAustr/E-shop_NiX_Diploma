56336248812360ffb8c7bf49349b76e6aaf29e20
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 12:57:38 +0200

    feature/added request classes

---
+++ Catalog.Host/Models/Requests/CreateCarRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.Host.Models.Requests
+{
+    public class CreateCarRequest
+    {
+        public string CarName { get; set; }
+        public string CarPromo { get; set; }
+        public int Price { get; set; }
+        public string ImageFileName { get; set; }
+        public int CarTypeId { get; set; }
+        public int ManufacturerId { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
