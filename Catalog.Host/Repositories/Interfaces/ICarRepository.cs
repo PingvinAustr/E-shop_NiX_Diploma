86af4984bf891a0194278b31b2b8a2c2fe001514
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 12:59:11 +0200

    feature/added car repo interface

---
+++ Catalog.Host/Repositories/Interfaces/ICarRepository.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Data.Entities;
+
+namespace Catalog.Host.Repositories.Interfaces
+{
+    public interface ICarRepository
+    {
+        Task<PaginatedItems<Car>> GetByPageAsync(int pageIndex, int pageSize);
+        Task<int?> Add(string name, string promo, int price, bool isAvailable, int carTypeId, int carManufaturerid, string imageFileName);
+        Task<PaginatedItems<Manufacturer>> GetBrands();
+    }
+}
