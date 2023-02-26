714e6f5a6737c1650901ec4a6a85525f35364930
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 12:57:58 +0200

    chore/updated program.cs

--- Catalog.Host/Program.cs
+++ Catalog.Host/Program.cs
@@ -1,23 +1,65 @@
-var builder = WebApplication.CreateBuilder(args);
+using Catalog.Host.Configurations;
+using Catalog.Host.Data;
+using Catalog.Host.Repositories;
+using Catalog.Host.Repositories.Interfaces;
+using Catalog.Host.Services;
+using Catalog.Host.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var configuration = GetConfiguration();
+
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.Configure<CatalogConfig>(configuration);
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddTransient<ICarRepository, CarRepository>();
+builder.Services.AddTransient<ICatalogService, CatalogService>();
+builder.Services.AddTransient<ICarService, CarService>();
+
+builder.Services.AddDbContextFactory<AppDbContext>(opts => opts.UseNpgsql(configuration["ConnectionString"]));
+builder.Services.AddScoped<IDbContextWrapper<AppDbContext>, DbContextWrapper<AppDbContext>>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapControllers();
+});
+
+CreateDbIfNotExists(app);
+app.Run();
+
+IConfiguration GetConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+    return builder.Build();
 }
 
-app.UseHttpsRedirection();
+void CreateDbIfNotExists(IHost host)
+{
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<AppDbContext>();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+            DbInitializer.Initialize(context).Wait();
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
+        }
+    }
+}
