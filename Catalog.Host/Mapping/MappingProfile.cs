3ebcf45dfbcb33e17df0268579ceac9b9dc77986
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-02-26 12:57:15 +0200

    feature/added mapping profile

---
+++ Catalog.Host/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Catalog.Host.Data.Entities;
+using Catalog.Host.Models.Dtos;
+
+namespace Catalog.Host.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Car, CarDto>()
+                .ForMember("ImageFileName", opt
+                    => opt.MapFrom<CarPictureResolver, string>(c => c.ImageFileName));
+            CreateMap<Manufacturer, ManufacturerDto>();
+            CreateMap<Data.Entities.Type, TypeDto>();
+        }
+    }
+}
