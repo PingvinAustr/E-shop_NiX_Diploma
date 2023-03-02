9ce7c52a42314e2bbd367c8db64fc55f21798090
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-03-02 08:26:38 +0200

    feature/added auth to program.cs & updated appsettings

--- Catalog.Host/Program.cs
+++ Catalog.Host/Program.cs
@@ -1,23 +1,65 @@
+using Catalog.Host;
 using Catalog.Host.Configurations;
 using Catalog.Host.Data;
+using Catalog.Host.Models.Requests;
 using Catalog.Host.Repositories;
 using Catalog.Host.Repositories.Interfaces;
 using Catalog.Host.Services;
 using Catalog.Host.Services.Interfaces;
-using Microsoft.EntityFrameworkCore;
 using FluentValidation.AspNetCore;
-using Catalog.Host.Models.Requests;
+using Infrastructure.Extensions;
+using Infrastructure.Filters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var configuration = GetConfiguration();
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers().AddFluentValidation(options =>
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(HttpGlobalExceptionFilter));
+}).AddFluentValidation(options =>
 {
     options.RegisterValidatorsFromAssemblyContaining<CreateCarRequest>();
+}).AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = true);
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "eShop- Catalog HTTP API",
+        Version = "v1",
+        Description = "The Catalog Service HTTP API"
+    });
+
+    var authority = configuration["Authorization:Authority"];
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows()
+        {
+            Implicit = new OpenApiOAuthFlow()
+            {
+                AuthorizationUrl = new Uri($"{authority}/connect/authorize"),
+                TokenUrl = new Uri($"{authority}/connect/token"),
+                Scopes = new Dictionary<string, string>()
+                {
+                    { "mvc", "website" },
+                    { "catalog.catalogitem", "catalog.catalogitem" }
+                }
+            }
+        }
+    });
+
+    options.OperationFilter<AuthorizeCheckOperationFilter>();
 });
 
+builder.AddConfiguration();
 builder.Services.Configure<CatalogConfig>(configuration);
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization(configuration);
+
 builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddTransient<ICarRepository, CarRepository>();
@@ -29,13 +71,35 @@ builder.Services.AddTransient<ITypeService, TypeService>();
 builder.Services.AddTransient<ICarService, CarService>();
 
 builder.Services.AddDbContextFactory<AppDbContext>(opts => opts.UseNpgsql(configuration["ConnectionString"]));
-builder.Services.AddScoped<IDbContextWrapper<AppDbContext>, DbContextWrapper<AppDbContext>>();
+builder.Services.AddScoped<Infrastructure.Services.Interfaces.IDbContextWrapper<AppDbContext>, Infrastructure.Services.DbContextWrapper<AppDbContext>>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "CorsPolicy",
+        builder => builder
+            .SetIsOriginAllowed((host) => true)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+});
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwagger()
+    .UseSwaggerUI(setup =>
+    {
+        setup.SwaggerEndpoint($"{configuration["PathBase"]}/swagger/v1/swagger.json", "Catalog.API V1");
+        setup.OAuthClientId("catalogswaggerui");
+        setup.OAuthAppName("Catalog Swagger UI");
+    });
+
 app.UseRouting();
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapDefaultControllerRoute();
